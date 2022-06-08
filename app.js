c67d3d41fa8e22deda882f9a6ee23d01538f6e55
Jaycee Porto <jaycee.porto@outlook.com>
2022-06-07 19:07:09 -0500

    Completed Exercise

--- app.js
+++ app.js
@@ -8,12 +8,36 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a -b;
+let multiply = a * b;
+let divide = a / b;
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+num + str ('1111')
+num + str2 ('11eleven')
+num + isPresent (12)
+firstName + num ('Frodo11')
+isPresent + str ('true11')
+firstName + lastName ('FrodoBaggins')
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+val == str (true)
+val === str (false)
+!isPresent (true)
+("eleven" == str2 && val >=str) (false)
+(!isPresent || isPresent) (true)
+0 == false (true)
+0 === false (false)
+0 != false (false)
+0 !== false (true)
